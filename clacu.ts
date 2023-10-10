c7e92a6a6e6b9b77ceb021389777baa26758503d
Huzaifa-Abdurahman <141252841+Huzaifa-Abdurahman@users.noreply.github.com>
2023-10-10 14:38:55 +0500

    Add files via upload

---
+++ clacu.ts
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+import chalk from "chalk"
+let {num1,num2,operation}= await inquirer.prompt([{
+    name: "num1",
+    type: "number",
+    message: "Enter first number:"},
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter secound number:"},
+        {
+            name: "operation",
+            type: "list",
+            choices:["add","sub","mul","div"],
+            message: "Enter first number:",
+        },
+]);
+//console.log(answers);
+//console.log("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old.");
+if(operation==="add"){
+    console.log(`the sum of tow numbers is = ${num1+num2}`)}
+    else if(operation==="sub"){
+        console.log(`the sum of tow numbers is = ${num1-num2}`)}
+        else if(operation==="mul"){
+            console.log(`the sum of tow numbers is = ${num1*num2}`)}
+            else if(operation==="div"){
+                console.log(`the sum of tow numbers is = ${num1/num2}`)}
